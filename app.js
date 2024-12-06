47fe7cee22256b3648c7b434297c70c019c20a22
i_dk <dalsbaekines@gmail.com>
2024-12-06 15:30:10 +0100

    rapport semaines suivantes

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const weeks = document.querySelectorAll(".week__dates"); // Sélectionne toutes les semaines
-  const days = document.querySelectorAll(".day-title"); // Sélectionne tous les jours
+  const weeks = document.querySelectorAll(".week__dates");
+  const days = document.querySelectorAll(".day-title");
 
-  // Cache toutes les semaines sauf les trois premières (1, 2, 3)
   weeks.forEach((week) => {
     if (
       !week.classList.contains("week__dates--week1") &&
       !week.classList.contains("week__dates--week2") &&
-      !week.classList.contains("week__dates--week3")
+      !week.classList.contains("week__dates--week3") &&
+      !week.classList.contains("week__dates--week4") &&
+      !week.classList.contains("week__dates--week5")
     ) {
       week.classList.add("hidden");
     }
   });
 
-  // Ajoute des événements pour afficher/masquer les jours d'une semaine
   weeks.forEach((week) => {
-    const weekDateText = week.querySelectorAll(".week__date-text"); // Récupère toutes les dates
+    const weekDateText = week.querySelectorAll(".week__date-text");
     weekDateText.forEach((dateText) => {
       dateText.addEventListener("click", () => {
-        const details = dateText.nextElementSibling; // Trouve le contenu des détails
+        const details = dateText.nextElementSibling;
         if (details && details.classList.contains("week__details")) {
           details.classList.toggle("hidden");
         }
@@ -28,10 +28,9 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 
-  // Ajoute des événements pour afficher/masquer le contenu d'un jour
   days.forEach((day) => {
     day.addEventListener("click", () => {
-      const targetClass = day.dataset.target; // Récupère la cible via data-target
+      const targetClass = day.dataset.target;
       const targetContent = document.querySelector(
         `.day-content.${targetClass}`
       );
